875b95d353b3c0b062e86b1b17697981877c4e20
Ricardokevins <3121416933@qq.com>
2020-09-12 19:53:08 +0800

    啦啦啦啦
    第一题的最优解代码完成
    最终结果10430

---
+++ code/MCTSv1.py
@@ -0,0 +1,226 @@
+import numpy as np
+from Draw import Draw
+
+map1 = np.array([[1, 8, 6, 3],
+                 [-1, 1, 2, 5],
+                 [-1, 2, 1, 3],
+                 [-1, -1, -1, 0]])
+
+states_list = []
+
+M = 1200  # 最大负重
+init_money = 10000
+base_water_price = 5
+base_water_weight = 3
+base_food_price = 10
+base_food_weight = 2
+
+base_consume_water = [5, 8, 10]
+base_consume_food = [7, 6, 10]
+
+# 晴朗 高温 沙暴
+weather = ["高温", "高温", "晴朗", "沙暴", "晴朗",
+           "高温", "沙暴", "晴朗", "高温", "高温",
+           "沙暴", "高温", "晴朗", "高温", "高温",
+           "高温", "沙暴", "沙暴", "高温", "高温",
+           "晴朗", "晴朗", "高温", "晴朗", "沙暴",
+           "高温", "晴朗", "晴朗", "高温", "高温"
+           ]
+
+
+def cost(cur_time, cur_state, next_state, cur_water, cur_food, states):
+    T = map1[cur_state][next_state]
+    last_time = cur_time + T
+    t = cur_time
+    if (t >= 30):
+        cur_water = -10000000
+        cur_food = -10000000
+        states.append('die')
+        return (last_time, cur_water, cur_food)
+    if cur_state == 1 and next_state == 1:  # 挖矿
+        if (weather[t] == '晴朗'):
+            cur_water -= base_consume_water[0] * 3
+            cur_food -= base_consume_food[0] * 3
+        elif (weather[t] == '高温'):
+            cur_water -= base_consume_water[1] * 3
+            cur_food -= base_consume_food[1] * 3
+        elif (weather[t] == '沙暴'):
+            cur_water -= base_consume_water[2] * 3
+            cur_food -= base_consume_food[2] * 3
+
+
+    elif (cur_state == next_state):  # 原地停留
+        if (weather[t] == '晴朗'):
+            cur_water -= base_consume_water[0]
+            cur_food -= base_consume_food[0]
+        elif (weather[t] == '高温'):
+            cur_water -= base_consume_water[1]
+            cur_food -= base_consume_food[1]
+        elif (weather[t] == '沙暴'):
+            cur_water -= base_consume_water[2]
+            cur_food -= base_consume_food[2]
+
+    else:  # 行走
+        while (t < last_time):
+            if (t >= 30):
+                break
+            if (weather[t] == '晴朗'):
+                cur_water -= base_consume_water[0] * 2
+                cur_food -= base_consume_food[0] * 2
+            elif (weather[t] == '高温'):
+                cur_water -= base_consume_water[1] * 2
+                cur_food -= base_consume_food[1] * 2
+            elif (weather[t] == '沙暴'):
+                cur_water -= base_consume_water[2]
+                cur_food -= base_consume_food[2]
+                last_time += 1
+            t += 1
+        if (t >= 30):
+            cur_water = -10000000
+            cur_food = -10000000
+            states.append('die')
+
+    return (last_time, cur_water, cur_food)
+
+
+def MC(cur_time, cur_state, cur_money, cur_water, cur_food, states):
+    states.append((cur_time, cur_state, cur_money, cur_water, cur_food))
+    if cur_water < 0 or cur_food < 0:
+        states_list.append(states)
+        return 0
+
+    if cur_money < 0:
+        states_list.append(states)
+        return 0
+
+    if cur_time >= 30:
+        # print(states)
+        states_list.append(states)
+        return 0
+
+    if cur_state == 3:  # 终点
+        # print("curwater----:", cur_water)
+        # print("curfood----:",cur_food)
+        states_list.append(states)
+        return cur_money + cur_food * base_food_price / 2 + cur_water * base_water_price / 2
+
+    if cur_state == 2:  # 村庄 买东西
+        if (np.random.uniform(0, 1) < 0.5):  # 买够到终点的钱
+            (cur_time, next_water, next_food) = cost(cur_time, cur_state, 3, cur_water, cur_food, states)  # 计算到终点的花费
+            if (cur_water < cur_water - next_water):
+                cur_water = cur_water - next_water
+                cur_money -= -next_water * base_water_price * 2
+            if (cur_food < cur_food - next_food):
+                cur_food = cur_food - next_food
+                cur_money -= -next_food * base_food_price * 2
+        else:  # 买到上限
+            wmax = min(cur_money / (base_food_price * 2 + base_water_price * 2),
+                       M / (base_food_weight * 2 + base_water_weight * 2))
+            fmax = wmax
+            if (cur_water < wmax):
+                cur_money -= (wmax - cur_water) * base_water_price * 2
+                cur_water = wmax
+            if (cur_food < fmax):
+                cur_money -= (fmax - cur_food) * base_food_price * 2
+                cur_food = fmax
+
+    next_state = np.random.choice(len(map1))
+    while map1[cur_state][next_state] == -1:
+        next_state = np.random.choice(len(map1))
+    if (cur_state == 1):  # 鼓励多挖矿
+        if (np.random.uniform() < 0.5):
+            next_state = 1
+
+    (next_time, next_water, next_food) = cost(cur_time, cur_state, next_state, cur_water, cur_food, states)
+
+    if cur_state == 0:
+        return MC(next_time,
+                  next_state,
+                  cur_money,
+                  next_water,
+                  next_food, states)
+    if cur_state == 2:  # 村庄
+        return MC(next_time,
+                  next_state,
+                  cur_money,
+                  next_water,
+                  next_food, states)
+
+    if cur_state == 1:  # 矿场
+        if (cur_state == 1 and next_state == 1):
+            return MC(next_time,
+                      next_state,
+                      cur_money + 1000,
+                      next_water,
+                      next_food, states)
+        else:
+            return MC(next_time,
+                      next_state,
+                      cur_money,
+                      next_water,
+                      next_food, states)
+
+
+def count_restMoney(i, j):
+    return 10000 - 5 * i - 10 * j
+
+
+def count_take(i, j):
+    return 1200 - 3 * i - 2 * j
+
+import Draw
+def Game():
+    print('Start')
+    iteration = 10000
+    money_list = []
+    sum_money = 0
+    cur_state=(0, 0, 7400, 240, 240)
+    while cur_state[1]!=3:
+        max_choice=1
+        max_score=-1
+        Choice={}
+
+        for j  in range( len(map1)):
+            Choice[j] = []
+            if map1[cur_state[1]][j] == -1:
+                continue
+            temp=[]
+            (next_time, next_water, next_food) = cost(cur_state[0], cur_state[1], j, cur_state[3], cur_state[4], temp)
+            new_state=(next_time,j,cur_state[2],next_water,next_food)
+            sum_money=0
+            for i in range(iteration):
+                states = []
+                sum_money += MC(new_state[0],new_state[1],new_state[2], new_state[3], new_state[4], states)
+                Choice[j].append(sum_money/(i+1))
+            print(j,sum_money/iteration)
+            if sum_money/iteration>max_score:
+                max_choice=j
+                max_score=sum_money/iteration
+        print("State",max_choice)
+        (next_time, next_water, next_food) = cost(cur_state[0], cur_state[1], j, cur_state[3], cur_state[4], temp)
+        cur_state = (next_time, j, cur_state[2], next_water, next_food)
+        Lines=[]
+        for i in Choice:
+            Lines.append(Choice[i])
+        print(len(Lines))
+        print(len(Lines[0]))
+        Draw.Draw(Lines, ['0', '1','2','3'], range(len(Lines[0])), "Test")
+    # if (cur_state == 1):  # 鼓励多挖矿
+    #     if (np.random.uniform() < 0.5):
+    #         next_state = 1
+    # for i in range(iteration):
+    #     states = []
+    #     sum_money = MC(0, 0, 7400, 240, 240, states)
+    #     money_list.append(sum_money)
+    # print(money_list)
+    # for i in states_list:
+    #     print(i)
+    # index = np.argmax(money_list)
+    # # print(index)
+    # print(max(money_list))
+    # print('--最优路径--', states_list[index])
+    # # print(states_list)
+    # # Draw([money_list],['测试'],range(len(money_list)),"MC")
+
+# [(0, 0), (8, 2), (10, 1), (11, 1), (12, 1), (13, 1), (14, 1), (16, 2), (20, 1), (21, 1), (22, 1), (28, 3)]
+Game()
--- code/题一最终代码.py
+++ code/题一最终代码.py
@@ -1,6 +1,7 @@
-init_water=167
-init_food=349
-money=5675
+init_water=180
+init_food=330
+money=10000-init_food*10-init_water*5
+
 
 weather=["高温","高温","晴朗","沙暴","晴朗",
          "高温","沙暴","晴朗","高温","高温",
@@ -49,61 +50,87 @@ base_food_weight=2
 
 def possess_c(cur_water,cur_food,cur_money,cur_day,log):
     can_take=1200-cur_water*3-cur_food*2
+    #print(can_take)
+    #print(cur_money)
     log=log+"At Day "+str(cur_day)+": "+"Reach c water and food "+str(cur_water)+" "+str(cur_food)+"\n"
-    for i in range(160,170):
-        for j in range(1):
-            #i=163
-            #j=0
-            if i*base_water_weight+j*base_food_weight<= can_take and i*base_water_price*2+j*base_food_price*2<= cur_money:
-                temp_water=cur_water+i
-                temp_food=cur_food+j
-                temp_money=cur_money-i*base_water_price*2-j*base_food_price*2
+    i=0
+    if can_take==1158:
+        temp_water=(108/3)+cur_water
+        temp_food=(38/2)+cur_food
+        temp_money = cur_money - (108/3)* base_water_price * 2-(38/2)*base_food_price*2
+    else:
+        i=int(can_take/base_water_weight)
+        temp_water=cur_water+i
+        temp_food=cur_food
+        temp_money=cur_money-i*base_water_price*2
 
-                newlog=log+"At Day "+str(cur_day)+": "+"Buy water and food "+str(i)+" "+str(j)+"\n"
-                q,w,e=go(cur_day,3)
-                temp_water1=temp_water-q
-                temp_food1=temp_food-w
-                newlog+="At Day "+str(e)+": "+"Move End water and food "+str(temp_water1)+" "+str(temp_food1)+"\n"
-                possess_z(temp_water1,temp_food1,temp_money,e,newlog)
+    newlog=log+"At Day "+str(cur_day)+": "+"Buy water and food "+str(i)+" "+"\n"
+    q,w,e=go(cur_day,3)
+    temp_water1=temp_water-q
+    temp_food1=temp_food-w
+    newlog+="At Day "+str(e)+": "+"Move End water and food "+str(temp_water1)+" "+str(temp_food1)+"\n"
+    possess_z(temp_water1,temp_food1,temp_money,e,newlog)
 
-                newlog = log+"At Day "+str(cur_day)+": "+"Buy water and food "+str(i)+" "+str(j)+ "\n"
-                q, w, e = go(cur_day, 2)
-                temp_water2 = temp_water - q
-                temp_food2 = temp_food - w
-                newlog += "At Day " + str(e) + ": " + "Move Mine water and food " + str(temp_water2) + " " + str(
-                    temp_food2) + "\n"
-                posseess_k(temp_water2, temp_food2, temp_money, e,newlog)
+    newlog = log+"At Day "+str(cur_day)+": "+"Buy water and food "+str(i)+ "\n"
+    q, w, e = go(cur_day, 2)
+    temp_water2 = temp_water - q
+    temp_food2 = temp_food - w
+    newlog += "At Day " + str(e) + ": " + "Move Mine water and food " + str(temp_water2) + " " + str(
+        temp_food2) + "\n"
+    posseess_k(temp_water2, temp_food2, temp_money, e,newlog)
 
 
 log_list={}
 def possess_z(cur_water,cur_food,cur_money,cur_day,log):
-    print("END ",cur_water*5/2+cur_food*10/2+cur_money,cur_day)
+    #print("END ",cur_water*5/2+cur_food*10/2+cur_money,cur_day)
     log+="End "+str(cur_day)+" "+str(cur_water*5/2+cur_food*10/2+cur_money)
+    if cur_water<0 or cur_food<0:
+        return -1
     log_list[log]=cur_water*5/2+cur_food*10/2+cur_money
     return cur_water*5/2+cur_food*10/2+cur_money
 
 def posseess_k(cur_water,cur_food,cur_money,cur_day,log):
-    water_limit=cur_water/(base_consume_water[get_weather(weather[cur_day-1])]*3)
-    food_limit=cur_food/(base_consume_food[get_weather(weather[cur_day-1])]*3)
+    log = log + "At Day " + str(cur_day) + ": " + "Reach M water and food " + str(cur_water) + " " + str(
+        cur_food) + "\n"
+    water_limit=cur_water/(base_consume_water[get_weather("晴朗")]*3)
+    food_limit=cur_food/(base_consume_food[get_weather("晴朗")]*3)
     total_limit=int(min(water_limit,food_limit))
     total_limit=min(total_limit,30-cur_day)
-    #print(total_limit)
-    for i in range(total_limit):
+
+    for i in range(1,total_limit+1):
         temp_food=cur_food
         temp_water=cur_water
         temp_day=cur_day
-        for j in range(i+1):
-            temp_water=temp_water-base_consume_water[get_weather(weather[cur_day+j-1])]*3
-            temp_food=temp_food-base_consume_food[get_weather(weather[cur_day+j-1])]*3
+        newlog=log
+        temp_money=cur_money
+
+        for j in range(1,i+1):
+            temp_water=temp_water-base_consume_water[get_weather(weather[cur_day+j-2])]*3
+            temp_food=temp_food-base_consume_food[get_weather(weather[cur_day+j-2])]*3
             temp_day+=1
-        newlog=log+"At Day " + str(temp_day) + ": " + "Dig " + str(i+1)+" Days "+str(temp_water) + " " + str(
-            temp_food) + "\n"
+            temp_money+=1000
+            newlog+="At Day " + str(temp_day) + ": " + "Dig " + str(j)+" Days "+str(temp_water) + " " + str(
+            temp_food) +" " + str(temp_money)+ "\n"
+
+
+        q, w, e = go(temp_day, 2)
+        if q < 0:
+            continue
+        temp_water2 = temp_water - q
+        temp_food2 = temp_food - w
+
+        if temp_food2 < 0 or temp_water2 < 0:
+            continue
+        newlog += "At Day " + str(e) + ": " + "Go Village water and food " + str(temp_water2) + " " + str(
+            temp_food2) + "\n"
+        possess_c(temp_water2, temp_food2, temp_money, e, newlog)
+
         q,w,e=go(temp_day,5)
         if q<0:
             continue
         temp_water1=temp_water-q
         temp_food1=temp_food-w
-        temp_money=1000* (i+1) +cur_money
+
 
         if temp_food1<0 or temp_water1<0:
             continue
@@ -112,23 +139,13 @@ def posseess_k(cur_water,cur_food,cur_money,cur_day,log):
             temp_food1) + "\n"
         possess_z(temp_water1,temp_food1,temp_money,e,newlog)
 
-        newlog = log+str(temp_day)+" " + "Dig " + str(i + 1) + " Day\n"
-        q, w, e = go(temp_day, 2)
-        if q < 0:
-            continue
-        temp_water2= temp_water-q
-        temp_food2 = temp_food-w
-        temp_money = 1000 * (i+1) + cur_money
-        if temp_food2<0 or temp_water2<0:
-            continue
-        newlog += "At Day " + str(e) + ": " + "Go Village water and food " + str(temp_water2) + " " + str(
-            temp_food2) + "\n"
-        possess_c(temp_water2, temp_food2, temp_money, e,newlog)
+
 
 
 q,w,e=go(1,6)
 log=""
-
+print(e)
+#exit()
 possess_c(init_water-q,init_food-w,money,e,log)
 
 max=-1
